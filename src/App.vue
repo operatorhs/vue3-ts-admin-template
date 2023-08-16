74f31404df9f0d0500ce26fa28db4747a9a09823
operatorhs <18112488219@163.com>
2023-08-16 19:26:37 +0800

    feat: first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>App</h1>
+    <div class="box">
+      <div class="con">cehi</div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped lang="scss">
+.box {
+  .con {
+    color: red;
+  }
+}
+</style>
